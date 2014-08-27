7a35699b1d6f72366be8c07d955b807191d50717
Kent Fredric <kentfredric@gmail.com>
2014-08-28 09:30:18 +1200

    Build results of 6879126 (on master)

--- lib/Dist/Zilla/Role/PluginLoader/Configurable.pm
+++ lib/Dist/Zilla/Role/PluginLoader/Configurable.pm
@@ -12,10 +12,7 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 use Moose::Role qw( has around with );
 use Dist::Zilla::Util;
 use Dist::Zilla::Util::ConfigDumper qw( config_dumper );
-with 'Dist::Zilla::Role::PrereqSource','Dist::Zilla::Role::PluginLoader' =>{ 
-  '-excludes' => [ 'logger', 'log','log_debug','log_fatal' ],
-};
-
+with 'Dist::Zilla::Role::PrereqSource','Dist::Zilla::Role::PluginLoader'; 
 
 
 
