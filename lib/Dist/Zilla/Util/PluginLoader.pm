5937112f931e895e94ce483c529b1dc2d0678670
Kent Fredric <kentfredric@gmail.com>
2014-08-28 13:12:36 +1200

    Build results of 1e7fe29 (on master)

--- lib/Dist/Zilla/Util/PluginLoader.pm
+++ lib/Dist/Zilla/Util/PluginLoader.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::PluginLoader;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Inflate a Legal Dist::Zilla Plugin from basic parts
 
@@ -115,7 +115,7 @@ Dist::Zilla::Util::PluginLoader - Inflate a Legal Dist::Zilla Plugin from basic 
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 SYNOPSIS
 
